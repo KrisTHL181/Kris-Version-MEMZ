227cb4642a14316b68cf149d1e34676fdb2765a6
Kris <KrisTHL181@outlook.com>
2023-06-16 20:26:20 +0800

    Add "Screen Melter" Function
    More Functions More Fun~

--- MEMZ.h
+++ MEMZ.h
@@ -528,3 +528,21 @@ void InvertColor_slow(){
 	}
 
 }
+
+void ScreenMelter(){
+	time_t t;
+	srand((unsigned) time(&t));
+	
+	HDC screenDC = GetDC(NULL);
+	
+	int scrWidth = GetDeviceCaps(screenDC, HORZRES);
+	int scrHeight = GetDeviceCaps(screenDC, VERTRES);
+	
+	int x;
+	while(1){
+		HDC screenDC = GetDC(NULL);
+		x = rand() % scrWidth;
+		BitBlt(screenDC, x, 1, 10, scrHeight, screenDC, x, 0, SRCCOPY);
+		Sleep(2);
+	}
+}
