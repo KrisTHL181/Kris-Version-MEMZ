99e3e05ec0e8b57cadf82ecb1683f41b6b2d6077
Kris <KrisTHL181@outlook.com>
2023-06-17 00:48:06 +0800

    Add "Shake" & "Round Corner" Function

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+    // SetConsoleRoundCorners(random_in(10,200),random_in(10,200),random_in(10,30)); // Setting Random Round Corners (Bugful)
     thread a(infclick); // Random Click
     thread b(infInvColor); // Invert Screen Color
     thread c(infIcon); // Random Draw Icons
@@ -15,5 +16,6 @@ int main(){
     thread i(infBlink); // Screen Blink
     thread j(InvertColor_slow); // Invert Screen Color (Slow) Like "Nepotonod"
     thread k(ScreenMelter); // Screen Melter Like "Nepotonod"
+    thread l(infShock); // Window Shocker
     scanf("%s"); // Pause Program
 }
