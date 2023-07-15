b9f567dd64e100652db02413a864fe2048ebdb92
Kris <KrisTHL181@outlook.com>
2023-07-16 00:24:43 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,40 +5,40 @@ using namespace std;
 
 int main()
 {
-	disable_redraw(); // Disable Redraw
-	StartWarning(); // Warning
-	CreateNote(); // Create Note
-	// Make Window On Top
-	Sleep(800); // Wait 0.8 Secounds(Anti Crash)...
-	// Main Process Zone //
-	thread a(infclick); // Random Click
-	thread b(infInvColor); // Invert Screen Color
-	thread c(infIcon); // Random Draw Icons
-	thread d(infCursor); // Random Move Mouse
-	thread e(infChange); // Invert String
-	thread f(inftunnel); // Draw Tunnel Effect
-	thread g(randomDraw); // Random Draw Lines
-	thread h(infplay); // Play System Error Sounds
-	thread i(infBlink); // Screen Blink
-	thread j(InvertColor_slow); // Invert Screen Color (Slow) Like "Nepotonod"
-	thread k(ScreenMelter); // Screen Melter Like "Nepotonod"
-	thread l(infShock); // Window Shocker
-	thread m(randomExecute); // Random Execute Some Programs/Websites
-	thread n(iconAura); // Draw Icons Around The Mouse
-	thread o(randomSeed); // Generate A New Random Seed Per 300ms
-	thread p(payloadMessageBox); // Pop Message Boxs
-	thread q(infglitch); // Glitch Screen
-	thread r(randomOutput); // Random Print Texts
-	thread s(rand_beep); // Random Beep
-	thread t(sayNyan); // Your computer was not trashed by the MEMZ Trojan. Now you can't enjoy the Nyan Cat~
-	thread u(OrientationsScreen); // Random Orientations Screens
-	thread v(InstallKeyboardHook); // Exit Program When Enter/Exit Pressed
-	thread w(InfRandWrite); // Random Write Text To Screen
-	/*
-	thread x(); // Waiting For Use
-	thread y(); // Waiting For Use
-	thread z(); // Waiting For Use
-	*/
-	getchar(); // Pause Program
-	return 0; // Normal Exit
+    disable_redraw(); // Disable Redraw
+    StartWarning(); // Warning
+    CreateNote(); // Create Note
+    SetFont(10); // Set Font Size To 10
+    Sleep(800); // Wait 0.8 Secounds(Anti Crash)...
+    // Main Process Zone //
+    thread a(infclick); // Random Click
+    thread b(infInvColor); // Invert Screen Color
+    thread c(infIcon); // Random Draw Icons
+    thread d(infCursor); // Random Move Mouse
+    thread e(infChange); // Invert String
+    thread f(inftunnel); // Draw Tunnel Effect
+    thread g(randomDraw); // Random Draw Lines
+    thread h(infplay); // Play System Error Sounds
+    thread i(infBlink); // Screen Blink
+    thread j(InvertColor_slow); // Invert Screen Color (Slow) Like "Nepotonod"
+    thread k(ScreenMelter); // Screen Melter Like "Nepotonod"
+    thread l(infShock); // Window Shocker
+    thread m(randomExecute); // Random Execute Some Programs/Websites
+    thread n(iconAura); // Draw Icons Around The Mouse
+    thread o(randomSeed); // Generate A New Random Seed Per 300ms
+    thread p(payloadMessageBox); // Pop Message Boxs
+    thread q(infglitch); // Glitch Screen
+    thread r(drawNyancat); // Draw Nyancat
+    thread s(rand_beep); // Random Beep
+    thread t(sayNyan); // Your computer was not trashed by the MEMZ Trojan. Now you can't enjoy the Nyan Cat~
+    thread u(OrientationsScreen); // Random Orientations Screens
+    thread v(InstallKeyboardHook); // Exit Program When Enter/Exit Pressed
+    thread w(InfRandWrite); // Random Write Text To Screen
+    /*
+    thread x(); // Waiting For Use
+    thread y(); // Waiting For Use
+    thread z(); // Waiting For Use
+    */
+    getchar(); // Pause Program
+    return 0; // Normal Exit
 }
